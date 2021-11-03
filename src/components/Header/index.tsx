20bd57a52b429c57c4fe08535a71b155807bd033
EdsonFer <edlivelifehd1z@gmail.com>
2021-11-02 22:19:14 -0300

    header and player components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,19 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
 export function Header() {
+	// yarn add date-fns para pegar a data
+	const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+		locale: ptBR,
+	});
+
 	return (
-		<header>
-			<img src="" alt="" />
+		<header className={styles.headerContainer}>
+			<img src="/logo.svg" alt="Podcastr" />
+			<p>O Melhor para você sempre</p>
+			<span>{currentDate}</span>
 		</header>
 	);
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Lexend:wght@500;600&display=swap"
